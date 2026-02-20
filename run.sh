81e8c74a29fd8fcb3f99f7e6c493b512cd38ba64
Pavel Vraj <57251717+pavelvraj@users.noreply.github.com>
2026-02-20 19:39:27 +0100

    Update run.sh to change app module path
    Removed directory creation for DB and updated module path.

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Zajistíme, že existuje složka pro DB v trvalém úložišti
-mkdir -p /config/streamcinema/data
-
 echo "Startuji StreamCinema API..."
-# Spustíme uvicorn
-# --app-dir /app říká, kde hledat main.py
-python3 -m uvicorn main:app --host 0.0.0.0 --port 8765
+# Spouštíme modul app.main a v něm proměnnou app
+python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8765
